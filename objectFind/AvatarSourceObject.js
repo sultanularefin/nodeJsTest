5f1893ae0aeb39a0621fd66677507c2cb4e8aef4
digitalarefin <digitalarefin@gmail.com>
2020-01-30 13:52:12 +0600

    commit

--- objectFind/AvatarSourceObject.js
+++ objectFind/AvatarSourceObject.js
@@ -29,9 +29,72 @@ const snapshot = {
 }
 
 
+const user = {
+    "_app":
+    {
+        "_automaticDataCollectionEnabled": true,
+        // "_deleteApp": [Function bound deleteApp], "_deleted": false,
+        "_initialized": true, "_name": "[DEFAULT]",
+        "_nativeInitialized": true,
+        "_options": {
+            "apiKey": "AIzaSyC0DQfj2MxnARXQcHw4I5IeY-Pzt-iVPH8",
+            "appId": "1:1042915152341:android:8422d5e6fa360bdaebed5c",
+            "databaseURL": "https://monos-e1b6c.firebaseio.com",
+            "gaTrackingId": null,
+            "messagingSenderId": "1042915152341",
+            "projectId": "monos-e1b6c",
+            "storageBucket": "monos-e1b6c.appspot.com"
+        }
+    },
+    "_authResult": true,
+    "_config": {
+        // "ModuleClass": [Function FirebaseAuthModule],
+        "hasCustomUrlOrRegionSupport": false,
+        "hasMultiAppSupport": true,
+        "namespace": "auth",
+        "nativeEvents": ["auth_state_changed", "auth_id_token_changed", "phone_auth_state_changed"],
+        "nativeModuleName": "RNFBAuthModule", "statics": {
+            // "AppleAuthProvider": [Function AppleAuthProvider],
+            // "EmailAuthProvider": [Function EmailAuthProvider],
+            // "FacebookAuthProvider": [Function FacebookAuthProvider],
+            // "GithubAuthProvider": [Function GithubAuthProvider],
+            // "GoogleAuthProvider": [Function GoogleAuthProvider],
+            // "OAuthProvider": [Function OAuthProvider],
+            // "PhoneAuthProvider": [Function PhoneAuthProvider],
+            "PhoneAuthState": [Object],
+            // "TwitterAuthProvider": [Function TwitterAuthProvider]
+        }, "version": "6.2.0"
+    },
+    "_customUrlOrRegion": undefined,
+    "_languageCode": null,
+    "_nativeModule": {
+        "APP_LANGUAGE": { "[DEFAULT]": null },
+        "APP_USER": {},
+        
+
+    },
+    "_settings": null,
+    "_user": {
+        "displayName": null,
+        "email": "mhmdarefin@gmail.com",
+        "emailVerified": false,
+        "isAnonymous": false,
+        "metadata": [Object],
+        "phoneNumber": null,
+        "photoURL": null,
+        "providerData": [Array],
+        "providerId": "firebase",
+        "uid": "I96lzpE2c9cghmxumWGNwP3o0Du2"
+    }
+}
+
+
+
 
 console.log("avatarSourceState: ", avatarSourceState);
 console.log("avatarSourceState.uri: ", avatarSourceState.uri);
 //  console.log("avatarSourceState[0].uri: ",avatarSourceState[0].uri);
+
+console.log("user.user.email:",user._user.email);
 
 
