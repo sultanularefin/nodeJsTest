2206d19795aa11d7b9457f08adb5687735c96da4
digitalarefin <digitalarefin@gmail.com>
2020-01-30 12:54:58 +0600

    commit before push

---
+++ objectFind/AvatarSourceObject.js
@@ -0,0 +1,37 @@
+let avatarSourceState =
+    { uri: 'file:///storage/emulated/0/Pictures/images/image-40d5aba6-3222-4377-a0f8-827d80bead94.jpg' }
+
+
+
+const snapshot = {
+    "bytesTransferred": 122755,
+    "metadata":
+    {
+        "bucket": "monos-e1b6c.appspot.com",
+        "cacheControl": "no-store",
+        "contentDisposition":
+            "inline; filename*=utf-8''mountains",
+        "contentEncoding": "identity",
+        "contentLanguage": null,
+        "contentType": "image/jpg",
+        "customMetadata": null,
+        "fullPath": "images/mountains",
+        "generation": "1580356541588002",
+        "md5Hash": "fvHVgGYQLaZxnz9fUD/yjw==",
+        "metageneration": "1",
+        "name": "mountains",
+        "size": 122755,
+        "timeCreated": "2020-10-06T03:55:41Z",
+        "updated": "2020-10-06T03:55:41Z"
+    },
+    "state": "success",
+    "totalBytes": 122755
+}
+
+
+
+console.log("avatarSourceState: ", avatarSourceState);
+console.log("avatarSourceState.uri: ", avatarSourceState.uri);
+//  console.log("avatarSourceState[0].uri: ",avatarSourceState[0].uri);
+
+
